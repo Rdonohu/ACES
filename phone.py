46f9f2b9dac78ad26ac634fc9558c2e3b2438ed7
Ronan Donohue <rdonohu1@jaguarlandrover.com>
2024-08-12 13:54:40 +0100

    add cabin view

--- phone.py
+++ phone.py
@@ -4,6 +4,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import io
 import folium
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+import os
+import cv2 
 
 class Ui_Phone( QtWidgets.QWidget):
     ToggleAlarmSignal = QtCore.pyqtSignal(int)
@@ -29,6 +31,9 @@ class Ui_Phone( QtWidgets.QWidget):
         self.ActivateAlarm = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.ActivateAlarm.setObjectName("ActivateAlarm ")
         self.verticalLayout.addWidget(self.ActivateAlarm)
+        self.CabinView = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.CabinView.setObjectName("CabinView")
+        self.verticalLayout.addWidget(self.CabinView)
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setGeometry(QtCore.QRect(0, 0, 491, 171))
         self.textBrowser.setObjectName("textBrowser")
@@ -45,6 +50,7 @@ class Ui_Phone( QtWidgets.QWidget):
         m.save(data, close_file=False)
         self.location = QWebEngineView(self.centralwidget)
         self.location.setHtml(data.getvalue().decode())
+        
         self.location.setGeometry(QtCore.QRect(0, 170, 481, 361))
         self.location.setObjectName("location")
         Phone.setCentralWidget(self.centralwidget)
@@ -55,6 +61,8 @@ class Ui_Phone( QtWidgets.QWidget):
         self.UpdateLocation.clicked.connect(self.updateLocation) 
         self.NotifyPolice.clicked.connect(self.notifyPolice)
         self.ActivateAlarm.clicked.connect(self.toggleAlarm)
+        self.CabinView.clicked.connect(self.CabinCapture)
+        self.cam = cv2.VideoCapture(0)
 
 
 
@@ -65,13 +73,24 @@ class Ui_Phone( QtWidgets.QWidget):
         self.UpdateLocation.setText(_translate("Phone", "Update Location"))
         self.NotifyPolice.setText(_translate("Phone", "Notify Police"))
         self.ActivateAlarm.setText(_translate("Phone", "Activate Alarm and Immobilizer"))
+        self.CabinView.setText(_translate("Phone", "Show The abin"))
         self.textBrowser.setHtml(_translate("Phone", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
         "p, li { white-space: pre-wrap; }\n"
         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:20pt; font-weight:400; font-style:normal;\">\n"
         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:19px; background-color:#ffffff;\"><span style=\" font-family:\'Consolas\',\'Courier New\',\'monospace\'; font-size:16pt; color:#a31515;\">If Your car has been Stolen please use the features below.</span></p></body></html>"))
     def updateLocation(self):
-        print("update")
+        coordinate = (52.6968598,-8.9031497)
+        m = folium.Map(
+            title = "Current Location",
+            zoom_start = 15,
+            location = coordinate
+        )
+
+        folium.Marker( location = coordinate, popup="Your JLR Vehicle").add_to(m)
+        data = io.BytesIO()
+        m.save(data, close_file=False)
+        self.location.setHtml(data.getvalue().decode())
 
     def notifyPolice(self):
         self.NotifyPoliceSignal.emit(1)
@@ -99,3 +118,10 @@ class Ui_Phone( QtWidgets.QWidget):
         "p, li { white-space: pre-wrap; }\n"
         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:20pt; font-weight:400; font-style:normal;\">\n"
         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:19px; background-color:#ffffff;\"><span style=\" font-family:\'Consolas\',\'Courier New\',\'monospace\'; font-size:16pt; color:#a31515;\">Your car has detected a break in, If this was not you please activate your desired actions below. There has been 5 minutes without action so police have been notified of the cars location.</span></p></body></html>")
+    
+    def CabinCapture(self):
+        ret, frame = self.cam.read()
+        img_name = "cabin_image.png"
+        cv2.imwrite(img_name, frame)
+        self.location.setHtml('<img src="cabin_image.png"/ style="width:100%; height:100%; object-fit:contain">', baseUrl=QtCore.QUrl.fromLocalFile(os.getcwd()+os.path.sep))
+
